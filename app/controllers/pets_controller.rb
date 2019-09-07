74005cd9dfd08841658881e50ea555668612b6a3
Loyda Lopez <loydalopez1@gmail.com>
2019-09-07 16:27:39 +0000

    creating and saving comments

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,20 @@ class PetsController < ApplicationController
 def index
   @pets = Pet.all
 end
+
+  def new
+    @pets = Pet.new
+  end 
+
+def create
+  Pet.create(pets_params)
+  redirect_to root_path
+end
+
+private
+
+def pets_params
+  params.require(:pets).permit(:name, :comment)
+end
+  
 end
