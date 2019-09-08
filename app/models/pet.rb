42c9de7a059661efaa112fc8e919703b590b15c1
Loyda Lopez <loydalopez1@gmail.com>
2019-09-08 17:44:20 +0000

    finishing touches

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,5 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 45, minimum: 3 }
+  validates :comment, presence: true, length: { maximum: 100, minimum: 10 }
+
 end
